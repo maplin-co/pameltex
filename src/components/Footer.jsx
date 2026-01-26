24b9b2fc934ed586dd7402f65905a1b93a4e58d8
rasali535 <maplininc@gmail.com>
2026-01-26 17:21:00 +0200

    Add link to web developer portfolio in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
                     Plot 18680 Khuhurutse St Phase 2, Gaborone</p>
                 <p className="crisis-text">If you are in a crisis or any other person may be in danger - don't use this site. <Link
                     to="/resources" style={{ color: 'var(--brand-teal)', fontWeight: 'bold' }}>These resources</Link> can provide you with immediate help.</p>
-                <p style={{ marginTop: '30px', fontSize: '11px', color: '#aaa' }}>Web dev by Ras Ali</p>
+                <p style={{ marginTop: '30px', fontSize: '11px', color: '#aaa' }}>
+                    Web dev by <a href="https://rasalibassist.themaplin.com/" target="_blank" rel="noopener noreferrer" style={{ color: '#aaa', textDecoration: 'underline' }}>Ras Ali</a>
+                </p>
             </div>
         </footer>
     );
